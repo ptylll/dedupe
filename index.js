7424a3947c51b243f554cd24d784ca987dd51abe
pantianyi <pantianyi@aliyun.com>
2022-03-22 14:23:32 +0800

    feat:调整登录样式

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function dedupe (client, hasher) {
 
     for (let i = 0; i < client.length; i++) {
         let elem = client[i]
-        let hashed = hasher(elem)
+            let hashed = hasher(elem)
 
         if (!lookup[hashed]) {
             clone.push(elem)
